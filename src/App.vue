b5f8838f7119100471d04aae229b948e9236998c
Bruno Lapierre <BrunoL@MiraGeoscience.local>
2022-01-18 11:18:41 -0500

    second commit

--- src/App.vue
+++ src/App.vue
@@ -5,41 +5,22 @@
         My Dashboard
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn text rounded>Home</v-btn>
-      <v-btn  text rounded>Login</v-btn>
+
+      <v-btn
+          v-for="link in links"
+          :key="`${link.label}-header-link`"
+          text
+          rounded
+          :to="link.url"
+      >
+        {{ link.label }}
+      </v-btn>
+<!--      <v-btn text rounded to="/">Home</v-btn>-->
+<!--      <v-btn  text rounded to="/login">Login</v-btn>-->
     </v-app-bar>
     <v-content>
-      <!--  Login Module -->
-      <v-card width="400" class="mx-auto mt-5">
-        <v-card-title>
-          <h1 class="display-1">Login</h1>
-        </v-card-title>
-        <v-card-text>
-          <v-form>
-            <v-text-field
-                label="Username"
-                prepend-icon="mdi-account-circle"
-            />
-            <v-text-field
-                :type="showPassword ? 'text' : 'password'"
-                label="Password"
-                prepend-icon="mdi-lock"
-                :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-                @click:append="showPassword = !showPassword"
-            />
-          </v-form>
-        </v-card-text>
-        <v-divider></v-divider>
-        <v-card-actions>
-          <v-btn color="success">
-            Register
-          </v-btn>
-          <v-spacer></v-spacer>
-          <v-btn color="info">
-            Login
-          </v-btn>
-        </v-card-actions>
-      </v-card>
+      <!--  Router Module -->
+      <router-view></router-view>
     </v-content>
     <v-main>
 <!--      <HelloWorld/>-->
@@ -56,13 +37,14 @@
       >
         <v-btn
             v-for="link in links"
-            :key="link"
+            :key="`${link.label}-footer-link`"
             color="white"
             text
             rounded
             class="my-2"
+            :to="link.url"
         >
-          {{ link }}
+          {{ link.label }}
         </v-btn>
         <v-flex
           primary
@@ -141,10 +123,20 @@ export default {
     // HelloWorld,
   },
 
-  data: () => ({
-    showPassword: false,
-    links: ['Home', 'Login'],
-  }),
+  data() {
+    return {
+      links: [
+        {
+          label: 'Home',
+          url: '/'
+        },
+        {
+          label: 'Login',
+          url: '/login'
+        }
+      ]
+    }
+  }
 };
 </script>
 
